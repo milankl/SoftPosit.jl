00b91ece93bcc0a5fa2164b32fdfeb825b6e31f9
milankl <milankloewer@gmx.de>
2019-04-24 19:07:39 +0100

    comparison and sqrt

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -18,6 +18,11 @@
 /(x::Posit16,y::Posit16) = ccall((:p16_div, SoftPositPath), Posit16, (Posit16,Posit16),x,y)
 /(x::Posit32,y::Posit32) = ccall((:p32_div, SoftPositPath), Posit32, (Posit32,Posit32),x,y)
 
+# Square root
+sqrt(x::Posit8) = ccall((:p8_sqrt, SoftPositPath), Posit8, (Posit8,),x)
+sqrt(x::Posit16) = ccall((:p16_sqrt, SoftPositPath), Posit16, (Posit16,),x)
+sqrt(x::Posit32) = ccall((:p32_sqrt, SoftPositPath), Posit32, (Posit32,),x)
+
 # Unary minus (multiply by posit -1)
 -(x::Posit8) = x*castP8(0xc0)
 -(x::Posit16) = x*castP16(0xc000)
@@ -31,3 +36,12 @@ zero(::Posit32) = castP32(0x00000000)
 one(::Posit8) = castP8(0x40)
 one(::Posit16) = castP16(0x4000)
 one(::Posit32) = castP32(0x40000000)
+
+# comparison
+<(x::Posit8,y::Posit8) = ccall((:p8_lt, SoftPositPath), Bool, (Posit8,Posit8),x,y)
+<(x::Posit16,y::Posit16) = ccall((:p16_lt, SoftPositPath), Bool, (Posit16,Posit16),x,y)
+<(x::Posit32,y::Posit32) = ccall((:p32_lt, SoftPositPath), Bool, (Posit32,Posit32),x,y)
+
+<=(x::Posit8,y::Posit8) = ccall((:p8_le, SoftPositPath), Bool, (Posit8,Posit8),x,y)
+<=(x::Posit16,y::Posit16) = ccall((:p16_le, SoftPositPath), Bool, (Posit16,Posit16),x,y)
+<=(x::Posit32,y::Posit32) = ccall((:p32_le, SoftPositPath), Bool, (Posit32,Posit32),x,y)

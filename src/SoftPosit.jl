da9deeb106fdf39b95380223fe59047a5f791688
milankl <milankloewer@gmx.de>
2019-04-30 18:41:30 +0100

    As a module

--- src/SoftPosit.jl
+++ src/SoftPosit.jl
@@ -1,6 +1,9 @@
-# module SoftPosit
-#
-# export AbstractPosit, Posit8, Posit16, Posit32
+module SoftPosit
+
+export AbstractPosit, Posit8, Posit16, Posit32,
+    Posit8_1, Posit16_1, Posit24_1,
+    Posit8_2, Posit16_2, Posit24_2,
+    maxpos,minpos,maxneg,minneg
 
 import Base: Float64, Float32, Float16, Int32, Int64,
     (+), (-), (*), (/), (<), (<=), (==), sqrt,
@@ -22,4 +25,4 @@ include("constants.jl")
 include("round.jl")
 include("print.jl")
 
-# end
+end

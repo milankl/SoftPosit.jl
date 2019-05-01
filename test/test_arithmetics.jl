a9815540aabc6ace7512e4767e7af36c18c2e236
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-05-01 22:09:09 +0100

    Add at-test macros to actually do the testing

--- test/test_arithmetics.jl
+++ test/test_arithmetics.jl
@@ -1,9 +1,15 @@
-f0,f1,f2,f3,f4 = 0.125,-0.5,1.75,2.5,0.0
+@testset "Arithmetics" begin
+    f0,f1,f2,f3,f4 = 0.125,-0.5,1.75,2.5,0.0
 
-# basic arithmetic operations
-Posit8(f0+f1*f2-f3) == Posit8(f0)+Posit8(f1)*Posit8(f2)-Posit8(f3)
-Posit16(f0/f1) == Posit16(f0)/Posit16(f1)
-Posit32(sqrt(f0)*f1/f3) == sqrt(Posit32(f0))*Posit32(f1)/Posit32(f3)
+    # basic arithmetic operations
+    @testset for T in (Posit8, Posit16, Posit32, Posit24_2)
+        @test T(f0 + f1 * f2 - f3) == T(f0) + T(f1) * T(f2) - T(f3)
+        @test T(f0 / f1) == T(f0) / T(f1)
+        @test T(sqrt(f0) * f1 / f3) == sqrt(T(f0)) * T(f1) / T(f3)
+        @test T(f1) ^ 2 == T(f1 ^ 2)
+        @test T(f3) ^ 5 == T(f3 ^ 5)
+    end
 
-# triggering complex infinity and conversion to inf
-Inf == Float64(Posit24_2(1.0)/Posit24_2(0.0))
+    # triggering complex infinity and conversion to inf
+    @test Inf == Float64(Posit24_2(1.0)/Posit24_2(0.0))
+end
--- test/test_conversions.jl
+++ test/test_conversions.jl
@@ -1,20 +1,22 @@
-x = 1.25
-i = 4
+@testset "Conversions" begin
+    x = 1.25
+    i = 4
 
-# Floats test conversion back and forth
-x == Float64(Posit8(x))
-x == Float64(Posit16(x))
-x == Float64(Posit32(x))
+    # Floats test conversion back and forth
+    @test x == Float64(Posit8(x))
+    @test x == Float64(Posit16(x))
+    @test x == Float64(Posit32(x))
 
-x == Float32(Posit8_2(x))
-x == Float32(Posit16_2(x))
-x == Float32(Posit24_2(x))
+    @test x == Float32(Posit8_2(x))
+    @test x == Float32(Posit16_2(x))
+    @test x == Float32(Posit24_2(x))
 
-# Posit to Posit back and forth
-Posit8(x) == Posit8(Posit16(x))
-Posit32(x) == Posit32(Posit16(Posit8_2(x)))
+    # Posit to Posit back and forth
+    @test Posit8(x) == Posit8(Posit16(x))
+    @test Posit32(x) == Posit32(Posit16(Posit8_2(x)))
 
-# Int to posit back and forth
-i == Int64(Posit8(i))
-i == Int64(Posit16(i))
-i == Int64(Posit32(i))
+    # Int to posit back and forth
+    @test i == Int64(Posit8(i))
+    @test i == Int64(Posit16(i))
+    @test i == Int64(Posit32(i))
+end

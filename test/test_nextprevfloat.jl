a8f0351afef2b6cd9ab8024a9b750068d816b4e0
milankl <milankloewer@gmx.de>
2019-05-13 16:20:46 +0100

    Next/prevfloat testing

---
+++ test/test_nextprevfloat.jl
@@ -0,0 +1,8 @@
+@testset "Nextfloat" begin
+    @testset for T in (Posit8, Posit16, Posit32,
+                    Posit8_1, Posit16_1, Posit24_1,
+                    Posit8_2, Posit16_2, Posit24_2)
+        @test -floatmax(T) == nextfloat(nextfloat(floatmax(T)))
+        @test -floatmin(T) == prevfloat(prevfloat(floatmin(T)))
+    end
+end

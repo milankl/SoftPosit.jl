b9ae2950c3fe94da841020956f826147a7574160
milankl <milankloewer@gmx.de>
2019-05-14 17:03:32 +0100

    parametric types partially undone

--- src/eps.jl
+++ src/eps.jl
@@ -11,4 +11,4 @@ eps(::Type{Posit8_2}) = Posit8_2(0x2800_0000)
 eps(::Type{Posit16_2}) = Posit16_2(0x0a00_0000)
 eps(::Type{Posit24_2}) = Posit24_2(0x0280_0000)
 
-eps(p::AbstractPosit) == max(p-prevfloat(p),nextfloat(p)-p)
+eps(p::AbstractPosit) = max(p-prevfloat(p),nextfloat(p)-p)
--- src/round.jl
+++ src/round.jl
@@ -3,5 +3,19 @@ round(x::Posit8) = ccall((:p8_roundToInt, SoftPositPath), Posit8, (Posit8,),x)
 round(x::Posit16) = ccall((:p16_roundToInt, SoftPositPath), Posit16, (Posit16,),x)
 round(x::Posit32) = ccall((:p32_roundToInt, SoftPositPath), Posit32, (Posit32,),x)
 
-round(x::T) where {T<:PositX1} =  ccall((:pX1_roundToInt, SoftPositPath), T, (T,),x)
-round(x::T) where {T<:PositX2} =  ccall((:pX2_roundToInt, SoftPositPath), T, (T,),x)
+# for Posits with 1 exponent bit
+for T in (:Posit8_1, :Posit16_1, :Posit24_1)
+    @eval begin
+        round(x::$T) =  ccall((:pX1_roundToInt, SoftPositPath), $T, ($T,),x)
+    end
+end
+
+# for Posits with 1 exponent bit
+for T in (:Posit8_2, :Posit16_2, :Posit24_2)
+    @eval begin
+        round(x::$T) =  ccall((:pX2_roundToInt, SoftPositPath), $T, ($T,),x)
+    end
+end
+
+# round(x::T) where {T<:PositX1} =  ccall((:pX1_roundToInt, SoftPositPath), T, (T,),x)
+# round(x::T) where {T<:PositX2} =  ccall((:pX2_roundToInt, SoftPositPath), T, (T,),x)

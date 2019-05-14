b8bbfa11eaeeaa940da65f09aaec2e241e8aaa12
milankl <milankloewer@gmx.de>
2019-05-14 13:55:45 +0100

    minusone function added

--- src/constants.jl
+++ src/constants.jl
@@ -1,25 +1,30 @@
-# literal zero and one (via reinterpretation of hexadecimal)
-# floatmax, floatmin
-
+# 8bit Posits
 one(::Type{T}) where {T<:PositAll8} = T(0x40)
+minusone(::Type{T}) where {T<:PositAll8} = T(0xc0)
 zero(::Type{T}) where {T<:PositAll8} = T(0x00)
 floatmax(::Type{T}) where {T<:PositAll8} = T(0x7f)
 floatmin(::Type{T}) where {T<:PositAll8} = T(0x01)
 -(x::T) where {T<:PositAll8} = x*T(0xc0)
 
+# 16bit Posits
 one(::Type{T}) where {T<:PositAll16} = T(0x4000)
+minusone(::Type{T}) where {T<:PositAll16} = T(0xc000)
 zero(::Type{T}) where {T<:PositAll16} = T(0x0000)
 floatmax(::Type{T}) where {T<:PositAll16} = T(0x7fff)
 floatmin(::Type{T}) where {T<:PositAll16} = T(0x0001)
 -(x::T) where {T<:PositAll16} = x*T(0xc000)
 
+# 24bit Posits
 one(::Type{T}) where {T<:PositAll24} = T(0x4000_0000)
+minusone(::Type{T}) where {T<:PositAll24} = T(0xc000_0000)
 zero(::Type{T}) where {T<:PositAll24} = T(0x0000_0000)
 floatmax(::Type{T}) where {T<:PositAll24} = T(0x7fff_ff00)
 floatmin(::Type{T}) where {T<:PositAll24} = T(0x0000_0100)
 -(x::T) where {T<:PositAll24} = x*T(0xc000_0000)
 
+# 32bit Posits
 one(::Type{Posit32}) = Posit32(0x4000_0000)
+minusone(::Type{Posit32}) = Posit32(0xc000_0000)
 zero(::Type{Posit32}) = Posit32(0x0000_0000)
 floatmax(::Type{Posit32}) = Posit32(0x7fff_ffff)
 floatmin(::Type{Posit32}) = Posit32(0x0000_0001)
@@ -63,7 +68,7 @@ function sign(p::Type{T}) where {T <: AbstractPosit}
         if ~isfinite(p)
             return notareal(T)
         else
-            return -one(T)
+            return minusone(T)
         end
     end
 end

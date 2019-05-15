3ec8ce195bc79336524d83344b1fc0f83c308d54
milankl <milankloewer@gmx.de>
2019-05-15 10:54:18 +0100

    Eps tests

--- test/test_sign.jl
+++ test/test_sign.jl
@@ -1,7 +1,8 @@
 @testset "Sign" begin
-    @testset for T in (Posit8, Posit16, Posit32)
-                    #Posit8_1, Posit16_1, Posit24_1,
-                    #Posit8_2, Posit16_2, Posit24_2)
+    @testset for T in (Posit8, Posit16, Posit32,
+                    # pX1_mul is not supported yet by C library
+                    # Posit8_1, Posit16_1, Posit24_1,
+                    Posit8_2, Posit16_2, Posit24_2)
 
         @test one(T) == sign(one(T))
         @test zero(T) == sign(zero(T))

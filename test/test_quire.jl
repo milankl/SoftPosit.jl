632f0ea936a265a770a972fd854858c246fd1704
milankl <milankloewer@gmx.de>
2019-05-23 15:25:02 +0100

    Quire test typo corrected

--- test/test_quire.jl
+++ test/test_quire.jl
@@ -19,10 +19,10 @@
     # Test 1e30 + 1 - 1e30 == 1
     # as for Posit32 1e30+1 = 1e6
     q = zero(Quire32)
-    q = fma(q,Posit16(1e30),one(Posit32))
-    q = fma(q,Posit16(1),one(Posit32))
-    q = fms(q,Posit16(1e30),one(Posit32))
-    @test one(Posit16) == Posit16(q)
+    q = fma(q,Posit32(1e30),one(Posit32))
+    q = fma(q,Posit32(1),one(Posit32))
+    q = fms(q,Posit32(1e30),one(Posit32))
+    @test one(Posit32) == Posit32(q)
 
     @test zero(Posit8) == Posit8(zero(Quire8))
     @test zero(Posit16) == Posit16(zero(Quire16))

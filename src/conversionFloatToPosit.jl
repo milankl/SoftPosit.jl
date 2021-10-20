ac9a5db8e00943f7bda73410c4035223fffc65ac
Milan <milankloewer@gmx.de>
2021-10-20 12:33:49 +0100

    make new conversion default

--- src/conversionFloatToPosit.jl
+++ src/conversionFloatToPosit.jl
@@ -1,39 +1,16 @@
 # from Float64 to Posit8,16,32
 Posit8(x::Float64) = ccall((:convertDoubleToP8, SoftPositPath), Posit8, (Float64,),x)
-Posit16(x::Float64) = ccall((:convertDoubleToP16, SoftPositPath), Posit16, (Float64,),x)
+Posit16_old(x::Float64) = ccall((:convertDoubleToP16, SoftPositPath), Posit16, (Float64,),x)
 Posit32(x::Float64) = ccall((:convertDoubleToP32, SoftPositPath), Posit32, (Float64,),x)
 
 # from Float32/16 to Posit8,16,32
 Posit8(x::T where {T <: Float16or32}) = Posit8(Float64(x))
-Posit16(x::T where {T <: Float16or32}) = Posit16(Float64(x))
 Posit32(x::T where {T <: Float16or32}) = Posit32(Float64(x))
 
-# from Float64 to Posit_1 (convertDoubleToPX1 currently not available)
-# Posit8_1(x::Float64) = ccall((:convertDoubleToPX1, SoftPositPath), Posit8_1, (Float64,Int64),x,8)
-# Posit16_1(x::Float64) = ccall((:convertDoubleToPX1, SoftPositPath), Posit16_1, (Float64,Int64),x,16)
-# Posit24_1(x::Float64) = ccall((:convertDoubleToPX1, SoftPositPath), Posit24_1, (Float64,Int64),x,24)
+Posit16(x::Float16) = Posit16(Float32(x))
+Posit16(x::Float64) = Posit16(Float32(x))
 
-# use detour
-Posit8_1(x::Float64) = Posit8_1(Posit32(x))
-Posit16_1(x::Float64) = Posit16_1(Posit32(x))
-Posit24_1(x::Float64) = Posit24_1(Posit32(x))
-
-# from Float32/16 to Posit_1
-Posit8_1(x::T where {T <: Float16or32}) = Posit8_1(Float64(x))
-Posit16_1(x::T where {T <: Float16or32}) = Posit16_1(Float64(x))
-Posit24_1(x::T where {T <: Float16or32}) = Posit24_1(Float64(x))
-
-# from Float64 to Posit_2
-Posit8_2(x::Float64) = ccall((:convertDoubleToPX2, SoftPositPath), Posit8_2, (Float64,Int64),x,8)
-Posit16_2(x::Float64) = ccall((:convertDoubleToPX2, SoftPositPath), Posit16_2, (Float64,Int64),x,16)
-Posit24_2(x::Float64) = ccall((:convertDoubleToPX2, SoftPositPath), Posit24_2, (Float64,Int64),x,24)
-
-# from Float32/16 to Posit_2
-Posit8_2(x::T where {T <: Float16or32}) = Posit8_2(Float64(x))
-Posit16_2(x::T where {T <: Float16or32}) = Posit16_2(Float64(x))
-Posit24_2(x::T where {T <: Float16or32}) = Posit24_2(Float64(x))
-
-function Posit16_new(x::Float32)
+function Posit16(x::Float32)
     ui = reinterpret(UInt32,x)	
     
     # REGIME AND EXPONENT BITS
@@ -64,3 +41,32 @@ function Posit16_new(x::Float32)
     p16 = signbit(x) ? -p16 : p16
     return reinterpret(Posit16,p16)
 end
+
+# legacy
+Posit16_old(x::Float32) = Posit16_old(Float64(x))
+Posit16_old(x::Float16) = Posit16_old(Float64(x))
+
+# from Float64 to Posit_1 (convertDoubleToPX1 currently not available)
+# Posit8_1(x::Float64) = ccall((:convertDoubleToPX1, SoftPositPath), Posit8_1, (Float64,Int64),x,8)
+# Posit16_1(x::Float64) = ccall((:convertDoubleToPX1, SoftPositPath), Posit16_1, (Float64,Int64),x,16)
+# Posit24_1(x::Float64) = ccall((:convertDoubleToPX1, SoftPositPath), Posit24_1, (Float64,Int64),x,24)
+
+# use detour
+Posit8_1(x::Float64) = Posit8_1(Posit32(x))
+Posit16_1(x::Float64) = Posit16_1(Posit32(x))
+Posit24_1(x::Float64) = Posit24_1(Posit32(x))
+
+# from Float32/16 to Posit_1
+Posit8_1(x::T where {T <: Float16or32}) = Posit8_1(Float64(x))
+Posit16_1(x::T where {T <: Float16or32}) = Posit16_1(Float64(x))
+Posit24_1(x::T where {T <: Float16or32}) = Posit24_1(Float64(x))
+
+# from Float64 to Posit_2
+Posit8_2(x::Float64) = ccall((:convertDoubleToPX2, SoftPositPath), Posit8_2, (Float64,Int64),x,8)
+Posit16_2(x::Float64) = ccall((:convertDoubleToPX2, SoftPositPath), Posit16_2, (Float64,Int64),x,16)
+Posit24_2(x::Float64) = ccall((:convertDoubleToPX2, SoftPositPath), Posit24_2, (Float64,Int64),x,24)
+
+# from Float32/16 to Posit_2
+Posit8_2(x::T where {T <: Float16or32}) = Posit8_2(Float64(x))
+Posit16_2(x::T where {T <: Float16or32}) = Posit16_2(Float64(x))
+Posit24_2(x::T where {T <: Float16or32}) = Posit24_2(Float64(x))

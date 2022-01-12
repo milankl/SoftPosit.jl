47241c6999fd04d3eb0e34a74c257341212b9ce4
Mosè Giordano <mose@gnu.org>
2022-01-12 18:57:25 +0100

    Use JLL package to provide the binary

--- src/SoftPosit.jl
+++ src/SoftPosit.jl
@@ -1,5 +1,10 @@
 module SoftPosit
 
+    import SoftPosit_jll
+    # For compatibility with previous versions of SofPosit.jl which used the name
+    # `SoftPositPath`.
+    const SoftPositPath = SoftPosit_jll.softposit
+
     export AbstractPosit, Posit8, Posit16, Posit32,
         Posit8_1, Posit16_1, Posit24_1,
         Posit8_2, Posit16_2, Posit24_2,
@@ -15,13 +20,6 @@ module SoftPosit
         nextfloat, prevfloat, fma,
         exp, exp2, exp10, log, log2, log10, cos, sin, tan,
         expm1,log1p
-
-    # Load in `deps.jl`, complaining if it does not exist
-    const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
-    if !isfile(depsjl_path)
-        error("SoftPosit not installed properly, run \"]build SoftPosit\", restart Julia and try again")
-    end
-    include(depsjl_path)
 
     include("typedef.jl")
     include("conversionFloatToPosit.jl")
